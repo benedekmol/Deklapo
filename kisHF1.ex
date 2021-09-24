b07e96b300910c1edaac1de30191ffcea3394bff
benedekmol <molnar.benedek.mate@gmail.com>
2021-09-24 23:51:33 +0200

    sit! bad order

--- kisHF1.ex
+++ kisHF1.ex
@@ -63,8 +63,9 @@ defmodule Khf1 do
     output = for elem <- Enum.at(input,k-1) do
       i ++ elem
     end
-    mergelist(input,acc ++ output, k-1)
+    mergelist(input, output ++ acc, k-1)
   end
+
   @spec mergelist(input::list):: output::list
   defp mergelist(input) do
     mergelist(input,[],length(input))
@@ -83,4 +84,12 @@ defmodule Khf1 do
   end
 end
 
-# IO.inspect Khf1.sit!(15)
+r1 = IO.inspect(Khf1.sit! 101)       === {0, []}
+r2 = IO.inspect(Khf1.sit! 15)        === {5, [{3, [{1, 4}, {2, 3}, {3, 2}, {4, 1}]}, {5, [{1, 1}]}]}
+r3 = IO.inspect(Khf1.sit! 14)        === {1, [{4, [{2, 1}]}]}
+r4 = IO.inspect(Khf1.sit! 10)        === {1, [{4, [{1, 1}]}]}
+r5 = IO.inspect(Khf1.sit! 9)         === {2, [{3, [{1, 2}, {2, 1}]}]}
+r6 = IO.inspect(Khf1.sit! 5)         === {0, []}
+# r7 = IO.inspect(Khf1.good_flocks 20) === [6, 9, 10, 12, 14, 15, 16, 18, 20]
+
+IO.puts "#{r1}, #{r2}, #{r3}, #{r4}, #{r5}, #{r6}"
