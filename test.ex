1ba8ccd13f5cf426cee3272f3190b450a68d53d7
benedekmol <molnar.benedek.mate@gmail.com>
2021-09-24 19:59:53 +0200

    tset

--- test.ex
+++ test.ex
@@ -24,14 +24,14 @@ defmodule Kaki do
 
 
   @spec check(k::integer, start::integer, sub::integer, sub_base::integer, iteratedtimes::integer) :: any()
-  defp check(k, start, sub, sub_base, iteratedtimes) when k < 0 do
-    false
+  defp check(k, _start, _sub, _sub_base, _iteratedtimes) when k < 0 do
+    [false, -1]
   end
-  defp check(0,start,sub, sub_base,iteratedtimes) do
+  defp check(0,_start,_sub, _sub_base,iteratedtimes) do
     if iteratedtimes > 2 do
-      true
+      [true, iteratedtimes]
     else
-      false
+      [false, -1]
     end
     # if iteratedtimes > 2 do
     #   {iteratedtimes, start, sub_base}
@@ -46,55 +46,89 @@ defmodule Kaki do
     check(k - subbing, start, subbing + sub_base, sub_base, iteratedtimes + 1 )
   end
 
-
   @spec check(k::integer, start::integer, sub::integer) :: any()
   def check(k, start, sub) do
     check(k, start, sub, sub, 0)
   end
 
-  @spec startiter(k::integer, start::integer, sub::integer) :: any()
-  defp startiter(k, start, sub) when start >= k do
-    :ok
-  end
-  defp startiter(k, start, sub) do
-    check(k,start,sub)
-    startiter(k, start + 1, sub)
-  end
-  @spec startiter(k::integer, sub::integer) :: any()
-  def startiter(k, sub) do
-    startiter(k, 1, sub)
-  end
+  # @spec startiter(k::integer, start::integer, sub::integer) :: any()
+  # defp startiter(k, start, sub) when start >= k do
+  #   :ok
+  # end
+  # defp startiter(k, start, sub) do
+  #   check(k,start,sub)
+  #   startiter(k, start + 1, sub)
+  # end
+  # @spec startiter(k::integer, sub::integer) :: any()
+  # def startiter(k, sub) do
+  #   startiter(k, 1, sub)
+  # end
 
-  @spec diffiter(k::integer, sub::integer) ::any()
-  def diffiter(k, sub) when sub > k do
-    :ok
-  end
-  def diffiter(k, sub) do
-    startiter(k, sub)
-    diffiter(k, sub + 1)
-  end
-  @spec diffiter(k::integer) :: any()
-  def diffiter(k) do
-    diffiter(k, 1)
-  end
+  # @spec diffiter(k::integer, sub::integer) ::any()
+  # def diffiter(k, sub) when sub > k do
+  #   :ok
+  # end
+  # def diffiter(k, sub) do
+  #   startiter(k, sub)
+  #   diffiter(k, sub + 1)
+  # end
+  # @spec diffiter(k::integer) :: any()
+  # def diffiter(k) do
+  #   diffiter(k, 1)
+  # end
 
-  @type variant :: { h :: integer, d :: integer }  ##utolsó sor db, sorok közötti diff
-  @type seatings :: { n :: integer, vs :: [ variant ] }  ## hány soros az ülés, variáns
-  @spec sit!( k :: integer ) :: r :: { m :: integer, ss :: [ seatings ] }
-  def sit!(k) do
+  # @type variant :: { h :: integer, d :: integer }  ##utolsó sor db, sorok közötti diff
+  # @type seatings :: { n :: integer, vs :: [ variant ] }  ## hány soros az ülés, variáns
+  # @spec sit!( k :: integer ) :: r :: { m :: integer, ss :: [ seatings ] }
+  # def sit!(k) do
 
-  end
+  # end
 
   # @spec chekker(k::integer, start::integer, diff::integer) :: any()
 
 
   @spec asdf(k::integer) :: any()
   def asdf(k) do
-    for start <- 1..k, diff <- 1..k, check(k,start,diff) do
-      {start, diff, check(k,start,diff)}
+    # for start <- 1..k, diff <- 1..k, check(k,start,diff) do
+    checkresult = for start <- 1..k, diff <- 1..k do
+      # {start, diff, check(k,start,diff)}
+      # Map.merge(%{startpoint: start, rowdiff: diff },check(k,start,diff))
+      check(k,start,diff) ++ [{start, diff}]
     end
+    listresult = for result <- checkresult, hd(result) do
+      tl(result)
+    end
+    # :lists.usort(listresult)
+    mapped = listresult
+    |> Enum.group_by(fn [x, _] -> x end, &Kernel.tl/1)
+    # |> Enum.reduce(%{}, &Kernel.tl/1)
+    # |> Enum.map(fn [x, y] -> {y} end)
+    for eqrow <- mapped  do
+      eqrow
+    end
+
   end
+
+  # @spec test(k::integer, x::integer):: any()
+  # def test(k,x) do
+  #   x = k
+  # end
+
 end
+
+
+# defmodule EnumHelpers do
+
+#   defp group_one({key, val}, categories) do
+#     Dict.update(categories, key, [val], &[val|&1])
+#   end
+
+#   def map_col_fast(coll) do
+#     Enum.reduce(coll, %{}, &group_one/2)
+#   end
+# end
+
+# IO.inspect EnumHelpers.map_col_fast([a: 2, a: 3, b: 3])
 
 
 # Kaki.check(9,1,2,2,0)
@@ -104,4 +138,8 @@ end
 # Kaki.startiter(15,4)
 
 # IO.inspect Kaki.diffiter(15)
-IO.inspect Kaki.asdf(15)
+IO.inspect Kaki.asdf(9)
+
+# IO.inspect is_truthy(1)
+
+# IO.inspect Kaki.test(3,x)
