f0a14768bac1b061721f5004ec6943fc1c22be3a
Molnar <benedek.molnar.ext@evosoft.com>
2021-09-29 22:31:17 +0200

    khf2 2

--- khf2.ex
+++ khf2.ex
@@ -1,48 +1,38 @@
 defmodule Khf2 do
 	@moduledoc """
 	Szövegelő
-	@author "Egyetemi Hallgató <egy.hallg@dp.vik.bme.hu>"
-	@date   "2021-10-01"
+	@author "Molnár Benedek molnar.benedek.mate@gmail.com"
+	@date   "2021-09-29"
 	...
 	"""
 
   @spec maxlength(list::List):: i::integer
-  def maxlength(list) do
-    max = 0
-    for elem <- list, max < length(elem) do
-      max = length(elem)
-    end
-    max
+  defp maxlength(list) do
+    max = Enum.max_by(list, fn x -> length(x) end)
+    length(max)
+  end
+
+  @spec addnil(list::List, max::integer):: any()
+  defp addnil(list, 0) do
+    list
+  end
+  defp addnil(list, maxminuslength) do
+    tmp = list ++ [nil]
+    addnil(tmp, maxminuslength - 1 )
   end
 
   @spec split(string::String):: list::List
-  def split(s) do
-    withoutnewline = String.replace(s, "\n", "")
-    String.split(withoutnewline, ~r/\ \ */)
+  defp split(s) do
+    withouttab = String.replace(s, "\t", "")
+    withoutnewline = String.replace(withouttab, "\n", "")
+
+    splitted = String.split(withoutnewline, ~r/\ \ */)
+    for split <- splitted, split != "" do
+      split
+    end
   end
 
-  @spec readfile(file::String) :: any()
-  def readfile(file) do
-    read = File.stream!(file)
-      # |> Stream.with_index
-      # |> Stream.map(fn {line, i} -> IO.puts "#{i+1} #{line}" end)
-      # |> Stream.map(fn (line) -> line end)
-      # |> Stream.run
-
-    listoflines = for line <- read, line != "\n" do
-      line
-    end
-    listofsplitlines = for line <- listoflines do
-      split(line)
-    end
-    max = maxlength(listofsplitlines)
-    IO.puts "max: #{max}"
-    # IO.inspect IEx.Info.info(tpye?)
-    # IO.inspect listoflines
-    IO.inspect listofsplitlines
-  end
-
-	# @spec szovegelo(file :: String.t) :: rss:: [[String.t | nil]]
+	@spec szovegelo(file :: String.t) :: rss:: [[String.t | nil]]
 	# A file fájl tartalmából előállítja az rss szövegmátrixot.
 	# A beolvasott szöveg és a szövegmátrix sorainak száma azonos.
 	# A szövegmátrix oszlopainak száma megegyezik a leghosszabb
@@ -51,8 +41,48 @@ defmodule Khf2 do
 	# több szóköz-jellegű karakter választja el. A sorokba balról
 	# jobbra haladva kerülnek be a szavak, a sorok végén üresen
 	# maradó cellákba pedig a nil atom.
+  def szovegelo(file) do
+    read = File.stream!(file)
+    # listoflines = for line <- read, line != "\n" do
+    listoflines = for line <- read, !String.match?(line, ~r/^[\ \t\n]*\n$/) do
+      line
+    end
+    if listoflines == [] do
+      []
+    else
+      listofsplitlines = for line <- listoflines do
+        split(line)
+      end
+      max = maxlength(listofsplitlines)
+      ## Add nil to lines
+      result = for line <- listofsplitlines do
+        if length(line) < max do
+          addnil(line, max - length(line))
+        else
+          line
+        end
+      end
+      result
+    end
+  end
+
+
+  @spec matchymatch(file::String):: any()
+  def matchymatch(file) do
+    read = File.stream!(file)
+    # listoflines = for line <- read, line != "\n" do
+    listoflines = for line <- read, !String.match?(line, ~r/^[\ \t\n]*\n$/) do
+      IO.puts "Line: "
+      IO.inspect line
+    end
+  end
 end
 
+# IO.inspect Khf2.matchymatch("./test.txt")
+
+
 # IO.inspect Khf2.readfile("./test.txt")
-Khf2.readfile("./test.txt")
+# IO.inspect Khf2.szovegelo("./test.txt")
+IO.inspect Khf2.szovegelo("./khf2_s0.txt")
 # IO.inspect Khf2.split("éalskjfd  asédlfkj jfjfjf\n")
+# IO.inspect Khf2.addnil(["a","b","c"],5)
