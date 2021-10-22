508e1fe769bf3c84a2ca913cdd9b742dc239b0f0
benedekmol <molnar.benedek.mate@gmail.com>
2021-10-22 04:11:08 +0200

    aésdlfkjaésldkfj

--- khf3.pl
+++ khf3.pl
@@ -76,36 +76,139 @@ does_alice_dance :-
 
 satrak_mx(NM, Fs, Ss, Mx) :-
   satrak_helper(NM, Fs, Ss, [], ResultArr),
-  write(X).
+  satornemfan(Fs, ResultArr),
+  writeln("FINAL: "),
+  writeln(ResultArr).
+
+sorepito(M, RowI, 0, Tmp, ResultArr, Row).
+
+sorepito(M, RowI, I, Tmp, ResultArr, Row) :-
+  I =< M,
+  I > 0,
+  \+ member([RowI,I], ResultArr),
+  T is I - 1,
+  write("not in it"),
+  sorepito(M, RowI, T, Tmp2, ResultArr, Te),
+  append([0], Te, Row);
+  I =< M,
+  I > 0,
+  member([RowI,I], ResultArr),
+  T is I - 1,
+  write("in it"),
+  sorepito(M, RowI, T, Tmp2, ResultArr, Te),
+  append([1], Te, Row).
+
+% kakker2(X,Y,Z) :-
+%   member([X,Y],Z).
+
+
+% sor(0, Tmp, ResultList) :-
+%   append(Tmp, [], ResultList).
+
+% sor(M, Tmp ,ResultList) :-
+%   M > 0,
+%   append([0], Tmp, TmpArr2),
+%   T is M - 1,
+%   sor(T, TmpArr2, ResultList).
+
+% ures_matrix(N-M, Mx) :-
+%  ures_matrix_helper(N,M,Mx).
+
+% ures_matrix_helper(0,M,Mx).
+
+% ures_matrix_helper(N,M,Mx) :-
+%   N > 0,
+%   N2 is N - 1,
+%   sor(M, [], Sor),
+%   % append([Sor], Mx, Tx),
+%   ures_matrix_helper(N2, M, Tx),
+%   append([Sor], Tx, Mx).
+
+% matrix_iro( [X,Y], Mx, Result) :-
+
+satornemfan(Fs, []) .
+
+satornemfan(Fs, [[X,Y] | ResultTail]) :-
+  write(X),
+  \+ member(Y-X, Fs),
+  satornemfan(Fs, ResultTail).
 
 satrak_helper(N-M, [], [], Tmp, Result) :-
   write("ARRRRR"),
-  write(Result),
-  write(Tmp),
-  Result is Tmp.
+  % write(Result),
+  % write(Tmp),
+  append(Tmp, [], Result),
+  write("Result: "), 
+  write(Result).
+  % Result is Tmp.
 
 %megnézi a i-j fához viszonyítva a direction jó-e ha igen akkor belerakja az eredménybe
 satrak_helper(N-M,[I-J | Tail], [Direction | DirectionTail ], TmpArr, ResultArr) :-
-  % Direction is "n",
+  %North Direction
+  Direction == n,
+  I =< N,
   I > 1,
   I =< N,
   J =< M,
   J > 0,
-  X is I - 1, 
-  Y is J,
+  Y is I - 1, 
+  X is J,
   \+ member([X,Y], TmpArr),
   append([[X,Y]], TmpArr, TmpArr2),
-  writeln(Direction),
-  satrak_helper(N-M,Tail, DirectionTail, TmpArr2, ResultArr),
-  write(TmpArr2),
-  write("X: "),
-  write(X),
-  write(" Y: "),
-  write(Y);
+  % writeln(Direction),
+  % format("X: ~w", X),
+  % format("Y: ~w", Y),
+  % writeln(""),
+  satrak_helper(N-M,Tail, DirectionTail, TmpArr2, ResultArr);
+  %East Direction
+  Direction == e,
+  J + 1 =< M,
+  J > 0,
+  I =< N,
+  I > 0,
+  Y is I,
+  X is J + 1,
+  \+ member([X,Y], TmpArr),
+  append([[X,Y]], TmpArr, TmpArr2),
+  % writeln(Direction),
+  % format("X: ~w", X),
+  % format("Y: ~w", Y),
+  % writeln(""),
+  satrak_helper(N-M,Tail, DirectionTail, TmpArr2, ResultArr);
+  %South Direction
+  Direction == s,
+  I > 0,
+  I + 1 =< N,
+  J > 0,
+  J =< M,
+  Y is I + 1,
+  X is J,
+  \+ member([X,Y], TmpArr),
+  append([[X,Y]], TmpArr, TmpArr2),
+  % writeln(Direction),
+  % format("X: ~w", X),
+  % format("Y: ~w", Y),
+  % writeln(""),
+  satrak_helper(N-M,Tail, DirectionTail, TmpArr2, ResultArr);
+  %West Direction
+  Direction == w,
+  I > 0,
+  I =< N,
+  J > 1,
+  J =< M,
+  Y is I,
+  X is J - 1,
+  \+ member([X,Y], TmpArr),
+  append([[X,Y]], TmpArr, TmpArr2),
+  % writeln(Direction),
+  % format("X: ~w", X),
+  % format("Y: ~w", Y),
+  % writeln(""),
+  satrak_helper(N-M,Tail, DirectionTail, TmpArr2, ResultArr);
   I < 0.
 
 kakker(X,Y,Z) :-
-  \+ member(X, Y).
+  member(X-Y,Z).
 
 
 length_list(N, L) :-
